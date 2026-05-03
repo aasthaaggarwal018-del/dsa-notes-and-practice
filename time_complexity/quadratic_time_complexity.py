abc0f1fc82bbeb11a760774dc4708b9249e3de1e
aastha <aasthaaggarwal018@gmail.com>
2026-05-03 16:50:50 +0530

    Create quadratic_time_complexity.py

---
+++ time_complexity/quadratic_time_complexity.py
@@ -0,0 +1,16 @@
+#o(n^2)
+
+n=5
+
+'''for i in range(n**2):
+    print(i)
+
+for i in range(n):
+    for j in range(n):
+        print(i,j)'''
+
+n=2
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            print(i,j,k)
