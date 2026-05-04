f23deb17091b7b87be1e6ee7ac71df3667fa8b28
aastha <aasthaaggarwal018@gmail.com>
2026-05-04 17:40:50 +0530

    Create tuple_operations.py

---
+++ list/(array)/tuple_operations.py
@@ -0,0 +1,15 @@
+tuple=(8,9.4,'hi','bye')
+
+# 1 len
+print(len(tuple))
+
+# 2 nagative index
+print(tuple[-2])
+
+# 3 clear
+tuple=list(tuple)
+tuple.clear()
+print(tuple)
+
+# other count,slicing,copy,index
+
