5025ba06078ef96494d1cf17b11a031b5354df22
aastha <aasthaaggarwal018@gmail.com>
2026-06-05 15:47:58 +0530

    Create dic_oprations.py

---
+++ list/(array)/dic_oprations.py
@@ -0,0 +1,17 @@
+dic = {1 : 'apple',
+       2 : 'banana',
+       3 : 'grape',
+       4 : 'orange'
+       }
+
+print(len(dic))
+
+#pop
+dic.pop(3)
+print(dic)
+
+# update 
+dic.update({ 3:'grapes'})
+print (dic)
+
+#get
